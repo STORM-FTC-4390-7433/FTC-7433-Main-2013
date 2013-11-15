c989bec360ebf31d9cab95e1183882940e4659ca
Justin Becker (Geekman7473) <geekman7473@gmail.com>
2013-11-15 17:44:46 -0500

    Hanging arm now moves to set point on single button press

--- hang.c
+++ hang.c
@@ -1,8 +1,28 @@
 #ifndef HANGSYS_C
 #define HANGSYS_C
-void updateHangSys (HangSys t, int motorPower){
-	motor[t.rightRaise] = motorPower;
-	motor[t.leftRaise] = motorPower;
+void updateHangSys (HangSys t, int motorPower, bool upButton, bool downButton){
+	if(t.direction != 0){
+		if(nSysTime - t.moveStartTime >= t.targetMoveTime ){
+			t.direction = 0;
+			motor[t.leftRaise] = 0;
+			motor[t.rightRaise] = 0;
+		}
+	}
+	if(t.direction == 0){
+		if(upButton ^ downButton){
+			if(upButton){
+				t.moveStartTime = nSysTime;
+				t.direction = 1;
+				motor[t.leftRaise] = motorPower;
+				motor[t.rightRaise] = motorPower;
+			} else {
+				t.moveStartTime = nSysTime;
+				t.direction = -1;
+				motor[t.leftRaise] = - motorPower;
+				motor[t.rightRaise] = - motorPower;
+			}
+		}
+	}
 	return;
 }
 #endif
--- hang.h
+++ hang.h
@@ -4,5 +4,11 @@ typedef struct{
 	//pointers to motors in the array motor[]
 	tMotor leftRaise,
 			rightRaise;
+	//value to hold the current SysTime at the moment that a movement operation begins
+	int moveStartTime;
+	//target length of time for the lift mechanism to move per movement operation
+	int targetMoveTime;
+	//direction of current travel 1 = up, -1 = down, 0 = stationary
+ int direction;
 }HangSys;
 #endif
