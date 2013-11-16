94fc6cd6093d64df94019603e69e943022974b35
geekman7473 <geekman7473@gmail.com>
2013-11-16 15:18:45 -0500

    end of Cherokee Competition

--- conveyor.c
+++ conveyor.c
@@ -2,8 +2,14 @@
 #define CONVEYOR_C
 
 void updateConvSys (ConvSys t, int driveMotorPower, int turnMotorPower){
-	motor[t.driveMotor] = driveMotorPower;
-	motor[t.turnMotor] = turnMotorPower;
+	t.iterations++;
+	if(t.iterations % 2 == 0){
+		motor[t.driveMotor] = driveMotorPower * 1.5;
+		motor[t.turnMotor] = turnMotorPower * 1.5;
+	} else {
+		motor[t.driveMotor] = driveMotorPower * 0.5;
+		motor[t.turnMotor] = turnMotorPower * 0.5;
+	}
 	return;
 }
 
--- hang.c
+++ hang.c
@@ -1,7 +1,7 @@
 #ifndef HANGSYS_C
 #define HANGSYS_C
 void updateHangSys (HangSys t, int motorPower, bool upButton, bool downButton){
-	if(t.direction != 0){
+	/*if(t.direction != 0){
 		if(nSysTime - t.moveStartTime >= t.targetMoveTime ){
 			t.direction = 0;
 			motor[t.leftRaise] = 0;
@@ -23,6 +23,17 @@ void updateHangSys (HangSys t, int motorPower, bool upButton, bool downButton){
 			}
 		}
 	}
-	return;
+	return; */
+		if(upButton){
+		motor[t.rightRaise] = motorPower;
+		motor[t.leftRaise] = motorPower;
+	} else if (downButton){
+		motor[t.rightRaise] = - motorPower;
+		motor[t.leftRaise] = - motorPower;
+	} else {
+		motor[t.rightRaise] = 0;
+		motor[t.leftRaise] = 0;
+		return;
+	}
 }
 #endif
