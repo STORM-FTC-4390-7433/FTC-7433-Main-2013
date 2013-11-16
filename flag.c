2f98ca4e021fbd35c9653df760cc04851b0e30a8
geekman7473 <geekman7473@gmail.com>
2013-11-15 21:31:16 -0500

    experimental

---
+++ flag.c
@@ -0,0 +1,14 @@
+#ifndef FLAGSYS_C
+#define FLAGSYS_C
+
+void updateFlagSys (FlagSys t, int motorPower, int tophat){
+	if(tophat == 0){
+		motor[t.drive] = motorPower;
+	} else if (tophat == 4){
+		motor[t.drive] = - motorPower;
+	} else {
+		return;
+	}
+}
+
+#endif
---
+++ flag.h
@@ -0,0 +1,11 @@
+#ifndef FLAGSYS_H
+#define FLAGSYS_H
+
+typedef struct {
+	//pointers to motors in the array motor[]
+	//frontLeft and frontRight should have motorEncoders mounted
+	tMotor drive;
+
+} FlagSys;
+
+#endif
