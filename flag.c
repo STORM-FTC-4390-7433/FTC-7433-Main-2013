30f6a4c881f9aefdd381fb6bf46a27355a934573
Justin Becker (Geekman7473) <geekman7473@gmail.com>
2013-11-21 17:15:29 -0500

    end of cherokee competition

--- flag.c
+++ flag.c
@@ -7,6 +7,7 @@ void updateFlagSys (FlagSys t, int motorPower, int tophat){
 	} else if (tophat == 4){
 		motor[t.drive] = - motorPower;
 	} else {
+		motor[t.drive] = 0;
 		return;
 	}
 }
--- hang.c
+++ hang.c
@@ -1,7 +1,7 @@
 #ifndef HANGSYS_C
 #define HANGSYS_C
 void updateHangSys (HangSys t, int motorPower, bool upButton, bool downButton){
-	if(t.direction != 0){
+	/*if(t.direction != 0){
 		if(nSysTime - t.moveStartTime >= t.targetMoveTime ){
 			t.direction = 0;
 			motor[t.leftRaise] = 0;
@@ -23,6 +23,29 @@ void updateHangSys (HangSys t, int motorPower, bool upButton, bool downButton){
 			}
 		}
 	}
-	return;
+	return; */
+		if(upButton){
+		motor[t.rightRaise] = motorPower;
+		motor[t.leftRaise] = motorPower;
+	} else if (downButton){
+		motor[t.rightRaise] = - motorPower;
+		motor[t.leftRaise] = - motorPower;
+	} else {
+		motor[t.rightRaise] = 0;
+		motor[t.leftRaise] = 0;
+		return;
+	}
 }
+<<<<<<< HEAD
+<<<<<<< HEAD
 #endif
+//This code makes the arm go up and down.
+//by:Justin
+//also gives the power to the motors Lines(4,5)  
+
+=======
+#endif
+>>>>>>> parent of ab77958... fixing git
+=======
+#endif
+>>>>>>> parent of ab77958... fixing git
